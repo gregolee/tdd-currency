07f81f6538d9c5a8cd435a21ca5089b28d378200
GregoLee <invinciblefunc@gmail.com>
2022-03-24 11:25:05 +0900

    feat($domain): ch03 모두를 위한 평등, 삼각측량 내용 반영
    삼각측량 방법 테스트 코드 작성 및 문서(markdown) 작성
    
    MoneyTest.java
    Dollar.java

--- src/test/java/domain/MoneyTest.java
+++ src/test/java/domain/MoneyTest.java
@@ -1,11 +1,13 @@
 package domain;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class MoneyTest {
 
 	@Test
+	@DisplayName("$5 X 2 = $10")
 	public void testMultiplication() {
 		Dollar five = new Dollar(5);
 		Dollar ten = five.times(2);
@@ -13,11 +15,29 @@ public class MoneyTest {
 	}
 
 	@Test
+	@DisplayName("Dollar 부작용(side effect)?")
 	public void testMultiplicationTwice() {
 		Dollar five = new Dollar(5);
 		Dollar product = five.times(2);
 		Assertions.assertEquals(10, product.amount);
 		product = five.times(3);
 		Assertions.assertEquals(15, product.amount);
+	}
+
+	@Test
+	@DisplayName("equals() : $5 == $5, $5 != $6")
+	public void testEquality() {
+		//Given
+		Dollar five = new Dollar(5);
+		Dollar fiveAnother = new Dollar(5);
+		Dollar six = new Dollar(6);
+
+		//When
+		boolean expectTrue = five.equals(fiveAnother);
+		boolean expectFalse = five.equals(six);
+
+		//Then
+		Assertions.assertEquals(true, expectTrue);
+		Assertions.assertEquals(false, expectFalse);
 	}
 }
